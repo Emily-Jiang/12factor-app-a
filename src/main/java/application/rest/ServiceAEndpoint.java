6ca777cd1411900cc589ea9e16c0205189d1d6ae
Andrew Rouse <anrouse@uk.ibm.com>
2018-10-05 11:32:38 +0100

    Cleanup imports

--- src/main/java/application/rest/ServiceAEndpoint.java
+++ src/main/java/application/rest/ServiceAEndpoint.java
@@ -7,8 +7,6 @@ import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
